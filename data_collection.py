374e3d363db53f9174aeebc2167146019720c9de
dpatt830 <120147120+dpatt830@users.noreply.github.com>
2023-06-09 21:01:14 -0500

    edited data_collection and created csv
    edited data_collection to create a csv based off of a data frame containing drug rank, name, and total prescriptions. Still need to add in drug price and edit df and csv.

--- data_collection.py
+++ data_collection.py
@@ -2,14 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-# initializing an overall list to store drug info
-drugs = []
+# initializing an lists to store drug info
+drug_info = []
+drug_prices = []
 
 # initializing a list to store hrefs
 href_list = []
 
 # establishing the url, requests page, and html parser object
-# looking at the top 50 most prescribed drugs in 2020; scraping price info
+# looking at the top 50 most commonly prescribed drugs; scraping price info
 url = f"https://clincalc.com/DrugStats/Top300Drugs.aspx"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
@@ -17,17 +18,30 @@ doc = BeautifulSoup(page, "html.parser")
 # looking at the text box which inlcudes names of the drugs
 tr_tags = doc.find_all('tr')[:51]
 
-# scraping each drugs' name and href
+# scraping the content in the td tags, creating a list that has the rank, price, and total prescriptions of the drug
+# list example: ['drug rank', 'Name', 'total prescriptions']
 for items in tr_tags:
+    tds = items.find_all('td')
+
+    # list will include additional info we do not need so only keep first 3 items
+    td_texts = [td.text for td in tds]
+    td_texts = td_texts[:3]
+    drug_info.append(td_texts)
+
+    # going through each a tag to get the href
     a_tags = items.find_all('a')
     for a_tag in a_tags:
-        name = a_tag.text
+       
+        # finding the hrefs of each drug to search for individual drug info later on
         href = a_tag['href']
+
+        # appending hrefs to list
         href_list.append(href)
 
-def cost_timeline(href):
-    # Function to scrape costs of specific drugs given their href
-    # over the timeline of 2013 - 2020
+    
+
+def cost(href):
+    # Function to scrape average costs of specific drugs given their href
 
     # establishing the url, requests page, and html parser object
     url = f'https://clincalc.com/DrugStats/{href}'
@@ -36,12 +50,33 @@ def cost_timeline(href):
 
     # finding at the chart displaying the different costs of the drug over the years
     # All the prices will be contained in this parent tag
-    div = doc.find_all('div', id="divPrescriptionCostOverTime")
+    tr_tag = doc.find_all('tr')[5]
 
-    # iterating each subsequent tag
-    for items in div:
+    tds = tr_tag.find_all('td')
 
-        # locating specifically 'tr' tags which contain the price strings
-        tr_tags = items.find_all('tr')[1:]
-        print(tr_tags)
-print(tr_tags)
+    # Intializing price variable
+    price = None
+
+    # iterating through the td texts in tds
+    for i in tds:
+
+        # If it is the price, our price variable's value is that
+        if '$' in i.text:
+            price = i.text
+
+            # removing the dollar sign to make the price a float variable
+            price = float(price[1:])
+            break
+    return price
+
+for href in href_list:
+
+    # appending to our main drug list, a list of drug details
+    drug_prices.append(cost(href))
+
+# creating a dataframe from our td_text list
+# we will later add onto the dataframe
+df = pd.DataFrame(drug_info, columns=['Drug Rank', 'Drug Name', 'Total Prescriptions'])
+
+# creating a csv of our df
+df.to_csv('Drug_Information.csv')
